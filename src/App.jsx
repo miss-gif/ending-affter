b8a8ad1d6636abd173c7997746f2afbdfa203e2f
miss-gif <ddc.akari@gmail.com>
2024-09-08 02:28:46 +0900

    Refactor : App.jsx 최적화 작업
    - React.lazy와 Suspense를 사용해서 페이지별로 필요한 컴포넌트만 로드되도록 해서 초기 로딩 시간을 최적화 작업함.
    - 중복 Route 요소 간소화
    - 라우트 경로 구조 개선

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,112 @@
+import { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
-import Accept from "./components/admin/Accept";
-import AcceptDetail from "./components/admin/AcceptDetail";
-import Ask from "./components/admin/Ask";
-import AskDetail from "./components/admin/AskDetail";
-import CategorySetting from "./components/admin/CategorySetting";
-import Report from "./components/admin/Report";
-import ReportDetail from "./components/admin/ReportDetail";
-import Home from "./components/ceo/Home";
-import LoginPageforCEO from "./components/ceo/LogintestforCeo";
-import OrdersAccepted from "./components/ceo/OrdersAccepted";
-import OrdersDetail from "./components/ceo/OrdersDetail";
-import OrdersHistory from "./components/ceo/OrdersHistory";
-import Reviews from "./components/ceo/Reviews";
-import Statistics from "./components/ceo/Statistics";
-import StoreManagement from "./components/ceo/StoreManagement";
-import AdminLayout from "./components/layout/AdminLayout";
-import CeoLayout from "./components/layout/CeoLayout";
-import RootLayout from "./components/layout/RootLayout";
-import MyPageOrderCloseDetail from "./components/user/mypage/MyPageOrderCloseDetail";
-import AuthCeoPage from "./pages/AuthCeoPage.tsx";
-import AuthPage from "./pages/AuthPage.tsx";
-import AuthUserPage from "./pages/AuthUserPage.tsx";
-import CeoPage from "./pages/CeoPage.jsx";
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage.tsx";
-import MyPage from "./pages/MyPage.tsx";
-import MyPageAddress from "./pages/MyPageAddress";
-import MyPageOrderClosePage from "./pages/MyPageOrderClosePage";
-import MyPageOrderPage from "./pages/MyPageOrderPage.tsx";
-import MyPageOrderPagee from "./pages/MyPageOrderPagee";
-import MyPageReviewPage from "./pages/MyPageReviewPage";
-import MypageReportPage from "./pages/MypageReportPage";
-import MypageUserWithdrawal from "./pages/MypageUserWithdrawal";
-import NotFound from "./pages/NotFound";
-import PaymentPage from "./pages/PaymentPage.jsx";
-import ProjectInfo from "./pages/ProjectInfo.jsx";
-// import RestaurantsPage from "./pages/RestaurantsPage.jsx";
-import StatisticsforAdmin from "./components/admin/StatisticsforAdmin";
-import CeoWithdrawal from "./components/ceo/CeoWithdrawal";
-import MenuManagement from "./components/ceo/MenuManagement";
-import StatisticsforCeo from "./components/ceo/StatisticsforCeo";
-import MypageLayout from "./components/layout/MypageLayout";
-import MyPageCouponPage from "./pages/MyPageCouponPage";
-import MyPageFavoriteListPage from "./pages/MyPageFavoriteListPage";
-import MyPageReportListPage from "./pages/MyPageReportListPage";
-import MypageReportDetailPage from "./pages/MypageReportDetailPage";
-import SnsLoginPage from "./pages/SnsLoginPage";
-import IntroPage from "./pages/intro/IntroPage";
 import { OrderProvider } from "./pages/user/OrderContext";
-import RestaurantDetailPage2 from "./pages/user/RestaurantDetailPage2";
-import RestaurantsPage from "./pages/user/main/RestaurantsPage";
+
+const Accept = lazy(() => import("./components/admin/Accept"));
+const AcceptDetail = lazy(() => import("./components/admin/AcceptDetail"));
+const Ask = lazy(() => import("./components/admin/Ask"));
+const AskDetail = lazy(() => import("./components/admin/AskDetail"));
+const CategorySetting = lazy(
+  () => import("./components/admin/CategorySetting"),
+);
+const Report = lazy(() => import("./components/admin/Report"));
+const ReportDetail = lazy(() => import("./components/admin/ReportDetail"));
+const StatisticsforAdmin = lazy(
+  () => import("./components/admin/StatisticsforAdmin"),
+);
+const CeoWithdrawal = lazy(() => import("./components/ceo/CeoWithdrawal"));
+const Home = lazy(() => import("./components/ceo/Home"));
+const LoginPageforCEO = lazy(() => import("./components/ceo/LogintestforCeo"));
+const MenuManagement = lazy(() => import("./components/ceo/MenuManagement"));
+const OrdersAccepted = lazy(() => import("./components/ceo/OrdersAccepted"));
+const OrdersDetail = lazy(() => import("./components/ceo/OrdersDetail"));
+const OrdersHistory = lazy(() => import("./components/ceo/OrdersHistory"));
+const Reviews = lazy(() => import("./components/ceo/Reviews"));
+const Statistics = lazy(() => import("./components/ceo/Statistics"));
+const StatisticsforCeo = lazy(
+  () => import("./components/ceo/StatisticsforCeo"),
+);
+const StoreManagement = lazy(() => import("./components/ceo/StoreManagement"));
+const AdminLayout = lazy(() => import("./components/layout/AdminLayout"));
+const CeoLayout = lazy(() => import("./components/layout/CeoLayout"));
+const MypageLayout = lazy(() => import("./components/layout/MypageLayout"));
+const RootLayout = lazy(() => import("./components/layout/RootLayout"));
+const MyPageOrderCloseDetail = lazy(
+  () => import("./components/user/mypage/MyPageOrderCloseDetail"),
+);
+const AuthCeoPage = lazy(() => import("./pages/AuthCeoPage.tsx"));
+const AuthPage = lazy(() => import("./pages/AuthPage.tsx"));
+const AuthUserPage = lazy(() => import("./pages/AuthUserPage.tsx"));
+const CeoPage = lazy(() => import("./pages/CeoPage.jsx"));
+const HomePage = lazy(() => import("./pages/HomePage"));
+const LoginPage = lazy(() => import("./pages/LoginPage.tsx"));
+const MyPage = lazy(() => import("./pages/MyPage.tsx"));
+const MyPageAddress = lazy(() => import("./pages/MyPageAddress"));
+const MyPageCouponPage = lazy(() => import("./pages/MyPageCouponPage"));
+const MyPageFavoriteListPage = lazy(
+  () => import("./pages/MyPageFavoriteListPage"),
+);
+const MyPageOrderClosePage = lazy(() => import("./pages/MyPageOrderClosePage"));
+const MyPageOrderPage = lazy(() => import("./pages/MyPageOrderPage.tsx"));
+const MyPageOrderPagee = lazy(() => import("./pages/MyPageOrderPagee"));
+const MyPageReportListPage = lazy(() => import("./pages/MyPageReportListPage"));
+const MyPageReviewPage = lazy(() => import("./pages/MyPageReviewPage"));
+const MypageReportDetailPage = lazy(
+  () => import("./pages/MypageReportDetailPage"),
+);
+const MypageReportPage = lazy(() => import("./pages/MypageReportPage"));
+const MypageUserWithdrawal = lazy(() => import("./pages/MypageUserWithdrawal"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+const PaymentPage = lazy(() => import("./pages/PaymentPage.jsx"));
+const ProjectInfo = lazy(() => import("./pages/ProjectInfo.jsx"));
+const SnsLoginPage = lazy(() => import("./pages/SnsLoginPage"));
+const IntroPage = lazy(() => import("./pages/intro/IntroPage"));
+const RestaurantDetailPage2 = lazy(
+  () => import("./pages/user/RestaurantDetailPage2"),
+);
+const RestaurantsPage = lazy(() => import("./pages/user/main/RestaurantsPage"));
 
 function App() {
   return (
     <OrderProvider>
       <div className="root-wrap">
-        <Routes>
-          {/* 로그인 및 회원가입 */}
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/auth">
-            <Route index element={<AuthPage />} />
-            <Route path="user" element={<AuthUserPage />} />
-            <Route path="ceo" element={<AuthCeoPage />} />
-          </Route>
-          <Route path="/oauth/redirect" element={<SnsLoginPage />} />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            {/* 로그인 및 회원가입 */}
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/auth">
+              <Route index element={<AuthPage />} />
+              <Route path="user" element={<AuthUserPage />} />
+              <Route path="ceo" element={<AuthCeoPage />} />
+            </Route>
+            <Route path="/oauth/redirect" element={<SnsLoginPage />} />
 
-          {/* 유저 */}
-          <Route path="/" element={<RootLayout />}>
-            <Route index element={<HomePage />} />
-            <Route path="restaurant/:id" element={<RestaurantsPage />} />
-            <Route path="restaurants/:id" element={<RestaurantDetailPage2 />} />
-            <Route path="payment/:id" element={<PaymentPage />} />
-            <Route path="projectinfo" element={<ProjectInfo />} />
-          </Route>
-
-          {/* 마이페이지 */}
-          <Route path="/" element={<MypageLayout />}>
-            <Route path="mypage">
-              <Route index element={<MyPage />} />
-              <Route path="order/:id" element={<MyPageOrderPage />} />
-              <Route path="order" element={<MyPageOrderPagee />} />
-              <Route path="report/list" element={<MyPageReportListPage />} />
-              <Route path="reportpage" element={<MypageReportPage />} />
+            {/* 유저 */}
+            <Route path="/" element={<RootLayout />}>
+              <Route index element={<HomePage />} />
+              <Route path="restaurant/:id" element={<RestaurantsPage />} />
               <Route
-                path="reportdetail/:id"
-                element={<MypageReportDetailPage />}
+                path="restaurants/:id"
+                element={<RestaurantDetailPage2 />}
               />
+              <Route path="payment/:id" element={<PaymentPage />} />
+              <Route path="projectinfo" element={<ProjectInfo />} />
+            </Route>
+
+            {/* 마이페이지 */}
+            <Route path="/mypage" element={<MypageLayout />}>
+              <Route index element={<MyPage />} />
+              <Route path="order">
+                <Route path=":id" element={<MyPageOrderPage />} />
+                <Route index element={<MyPageOrderPagee />} />
+              </Route>
+              <Route path="report">
+                <Route index element={<MyPageReportListPage />} />
+                <Route
+                  path="details/:id"
+                  element={<MypageReportDetailPage />}
+                />
+              </Route>
               <Route path="orderclose">
                 <Route index element={<MyPageOrderClosePage />} />
                 <Route path=":id" element={<MyPageOrderCloseDetail />} />
@@ -96,53 +117,50 @@ function App() {
               <Route path="coupon" element={<MyPageCouponPage />} />
               <Route path="favorite" element={<MyPageFavoriteListPage />} />
             </Route>
-          </Route>
 
-          {/* 사업자 */}
-          <Route path="/ceopage" element={<CeoLayout />}>
-            <Route index element={<CeoPage />} />
-            <Route path="home" element={<Home />} />
-            <Route path="login" element={<LoginPageforCEO />} />
-            <Route path="orders-accepted" element={<OrdersAccepted />} />
-            <Route path="orders-history" element={<OrdersHistory />} />
-            <Route
-              path="orders/details/:doneOrderPk"
-              element={<OrdersDetail />}
-            />
-            <Route path="reviews" element={<Reviews />} />
-            <Route path="store-management" element={<StoreManagement />} />
-            <Route path="menu-management" element={<MenuManagement />} />
-            <Route path="statistics" element={<Statistics />} />
-            <Route path="statisticsforCeo" element={<StatisticsforCeo />} />
-
-            <Route path="withdraw" element={<CeoWithdrawal />} />
-            <Route path="logins" element={<LoginPageforCEO />} />
-          </Route>
-
-          {/* 관리자 */}
-          <Route path="/admin" element={<AdminLayout />}>
-            <Route index element={<Accept />} />
-            <Route path="category-setting" element={<CategorySetting />} />
-            <Route path="statistics" element={<StatisticsforAdmin />} />
-
-            <Route path="ask">
-              <Route index element={<Ask />} />
-              <Route path="details/:id" element={<AskDetail />} />
+            {/* 사업자 */}
+            <Route path="/ceopage" element={<CeoLayout />}>
+              <Route index element={<CeoPage />} />
+              <Route path="home" element={<Home />} />
+              <Route path="login" element={<LoginPageforCEO />} />
+              <Route path="orders-accepted" element={<OrdersAccepted />} />
+              <Route path="orders-history" element={<OrdersHistory />} />
+              <Route
+                path="orders/details/:doneOrderPk"
+                element={<OrdersDetail />}
+              />
+              <Route path="reviews" element={<Reviews />} />
+              <Route path="store-management" element={<StoreManagement />} />
+              <Route path="menu-management" element={<MenuManagement />} />
+              <Route path="statistics" element={<Statistics />} />
+              <Route path="statisticsforCeo" element={<StatisticsforCeo />} />
+              <Route path="withdraw" element={<CeoWithdrawal />} />
             </Route>
-            <Route path="report">
-              <Route index element={<Report />} />
-              <Route path="details/:report_pk" element={<ReportDetail />} />
-            </Route>
-            <Route path="accept">
+
+            {/* 관리자 */}
+            <Route path="/admin" element={<AdminLayout />}>
               <Route index element={<Accept />} />
-              <Route path="details/:resPk" element={<AcceptDetail />} />
+              <Route path="category-setting" element={<CategorySetting />} />
+              <Route path="statistics" element={<StatisticsforAdmin />} />
+              <Route path="ask">
+                <Route index element={<Ask />} />
+                <Route path="details/:id" element={<AskDetail />} />
+              </Route>
+              <Route path="report">
+                <Route index element={<Report />} />
+                <Route path="details/:report_pk" element={<ReportDetail />} />
+              </Route>
+              <Route path="accept">
+                <Route index element={<Accept />} />
+                <Route path="details/:resPk" element={<AcceptDetail />} />
+              </Route>
             </Route>
-          </Route>
 
-          {/* 공통 */}
-          <Route path="*" element={<NotFound />} />
-          <Route path="/intro" element={<IntroPage />} />
-        </Routes>
+            {/* 공통 */}
+            <Route path="*" element={<NotFound />} />
+            <Route path="/intro" element={<IntroPage />} />
+          </Routes>
+        </Suspense>
       </div>
     </OrderProvider>
   );
